6dd5e906b8f52b1f6c5b134bc7ce5e0cedb3a467
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-14 17:24:56 -0500

    Dashboard and Display component tests are complete.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from "react"
+import { render, queryByText } from "@testing-library/react"
+import Display from "./Display"
+
+test('Display renders to screen', () => {
+    render(<Display />)
+})
+
+test(`Display 'Locked' if the locked prop is true, and 'Unlocked' if false`, () => {
+    const { getByText, queryByText } = render(<Display locked={false} />)
+    expect(getByText(/Unlocked/i))
+    expect(queryByText(/^Locked/i)).toBeFalsy();
+    expect(getByText(/Unlocked/i).classList.contains('green-led')).toBe(true);
+})
+
+test(`Display 'Closed' if the closed prop is true, and 'Open' if false`, () => {
+    const { getByText, queryByText } = render(<Display closed={true} />)
+    expect(getByText(/Closed/i))
+    expect(queryByText(/Open/i)).toBeFalsy();
+    expect(getByText(/Closed/i).classList.contains('red-led')).toBe(true)
+})
